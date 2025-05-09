de99ceccd47d805008288578ced4a286499e47d0
Khushi Maheshwari <khushimaheshwari869@gmail.com>
2025-05-09 14:50:25 +0530

    updated about me api added

--- routers/user.routes.js
+++ routers/user.routes.js
@@ -9,5 +9,5 @@ const { updateLoggedInUser } = require("../controllers/user.controller");
 router.get("/others", authMiddleware, userController.getOtherUsers);
 router.post("/like", authMiddleware, likeController.likeUser);
 router.get("/me", authMiddleware, userController.getLoggedInUser);
-router.put("/me", authMiddleware, updateLoggedInUser);
+router.post("/me", authMiddleware, updateLoggedInUser);
 module.exports = router;
